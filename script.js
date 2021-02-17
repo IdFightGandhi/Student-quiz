0c1f9bf9361ab33c8da68f1797789f94dabbba00
William Thorne <williamcthorne@gmail.com>
2021-02-16 20:27:48 -0800

    html skeleton, JS first pass

---
+++ script.js
@@ -0,0 +1,78 @@
+// make an array of questions
+var myQuestions = [
+    {
+        question1: "What is the most annoying TV series ever written?",
+        answers: {
+
+            a: "Gossip Girl",
+            b: "How I Met Your Mother",
+            c: "The Big Bang Theory",
+            d: "A guy on Public Access screaming at you",
+            e: "Game of Thrones"
+        },
+        correctAnswer: 'e',
+
+        question2: "Who was the worst person in the popular Netflix documentary, 'Tiger King'",
+        answers: {
+
+            a: "Carole Baskin",
+            b: "Joe Exotic",
+            c: "Allen Glover",
+            d: "That weird guy who owned the other Exotic Zoo but like, it was a sex cult...?",
+            e: "Game of Thrones"
+        },
+        correctAnswer: 'e',
+
+        question3: "What is the worst flavor of Gatorade?",
+        answers: {
+
+            a: "Cherry",
+            b: "Cucumber-Lime",
+            c: "Blueberry",
+            d: "Strrawberry",
+            e: "Game of Thrones"
+        },
+        correctAnswer: 'e',
+
+        question4: "Which of these is a primary cause of Climate Change?",
+        answers: {
+
+            a: "Car Emissions",
+            b: "Transport Ships",
+            c: "Industrial Farms",
+            d: "Fossil Fuels",
+            e: "Game of Thrones"
+        },
+        correctAnswer: 'e',
+
+    }
+];
+
+function showQuiz(questions, quizContainer){
+    //this will store answer choices and show the quiz
+    
+}
+
+
+
+
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitBtn){
+    function showQuestions(questions, quizContainer){
+        //questions here
+    }
+
+}
+
+function showResults(questions, quizContainer){
+    // this will contain the user's choces and display score at the end
+}
+
+showQuestions(questions, quizContainer);
+
+//button to submit answers and get score
+submitButton.onclick = function(){
+    showResults(questions,quizContainer, resultsContainer);
+}
+
+
