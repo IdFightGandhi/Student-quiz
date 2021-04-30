9f6422483a4b66504a54edd8ea1ccf25e62d54a9
William Thorne <williamcthorne@gmail.com>
2021-04-30 13:34:51 -0700

    questions

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var questions = [
+    {
+        Question:"Which bear is best?",
+        Answers:["Black Bear","Brown Bear","Polar Bear","Battlestar Galactica"],
+        Correct:"Battlestar Galactica"
+    },
+    {
+        Question:"Why is Game of Thrones the best TV show ever?",
+        Answers:["Writing","Characters","Nudity","It Isn't"],
+        Correct:"It Isn't"
+    },
+    {
+        Question:"Which of these is not a city?",
+        Answers:["Seattle","Shanghai","Sidney","Weed"],
+        Correct:["Sidney"]
+    },
+    {
+        Question:"How many licks does it take to get to the center of a tootsie-roll pop?",
+        Answers:["415","364","522","The World May Never Know"],
+        Correct:["Battlestar Galactica"]
+    },
+    
+]
